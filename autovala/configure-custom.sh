cc52ac1ce84a75d6a7ebb6ebc54a400abddc08d7
Chris Cromer <chris@cromer.cl>
2017-10-11 19:16:07 -0300

    fix missing cmake flag with configure wrapper

--- autovala/configure-custom.sh
+++ autovala/configure-custom.sh
@@ -2,5 +2,6 @@
 
 ENABLE_VARS="kde-tray|ON|KDE_TRAY"
 ENABLE_KDE_TRAY_DOC="build with kde tray and appindicator instead of gtk3 tray"
-DISABLE_VARS="aur|ON|DISABLE_AUR"
+DISABLE_VARS="aur|ON|DISABLE_AUR icon-update|OFF|ICON_UPDATE"
 DISABLE_DISABLE_AUR_DOC="disable the AUR in pamac"
+DISABLE_ICON_UPDATE_DOC="disable the update of the icon cache after installing"
